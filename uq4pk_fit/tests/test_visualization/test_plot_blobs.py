63c50f6bb356a6a1bf4d77c148fcda9bce0f59be
Fabian Parzer <fabian.kai.parzer@univie.ac.at>
2022-03-09 15:11:29 +0100

    Removed dependency on CV2 and updated notebook.

---
+++ uq4pk_fit/tests/test_visualization/test_plot_blobs.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+
+from uq4pk_fit.blob_detection.detect_blobs import detect_blobs
+from uq4pk_fit.visualization import plot_blobs
+
+
+def test_plot_blobs():
+    test_img = np.loadtxt("map.csv", delimiter=",")
+    sigma_scale = np.arange(1, 16)
+    sigma_list = [np.array([0.5 * sigma, sigma]) for sigma in sigma_scale]
+    blobs = detect_blobs(image=test_img, sigma_list=sigma_list)
+    matched_pairs = [tuple([blob, None]) for blob in blobs]
+    plot_blobs(image=test_img, blobs=matched_pairs, )
