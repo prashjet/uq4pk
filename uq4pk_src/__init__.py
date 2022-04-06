120808e710467d8f73bb23133280438ebde1ddc3
prashjet <prashinjethwa@gmail.com>
2022-04-06 19:45:06 +0200

    Add svd_mcmc to the uq4pk_src codebase

--- uq4pk_src/__init__.py
+++ uq4pk_src/__init__.py
@@ -1,1 +1,1 @@
-from uq4pk_src import model_grids, plotting, losvds, distribution_function, data, observation_operator, regularisers
+from uq4pk_src import model_grids, plotting, losvds, distribution_function, data, observation_operator, regularisers, svd_mcmc
