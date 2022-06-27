344c77f336c44a73026b6084076d4b23a097ebb3
Fabian Parzer <fabian.kai.parzer@univie.ac.at>
2022-06-27 07:50:47 +0200

    No, for now svd_mcmc will be in uq4pk_src.

--- uq4pk_src/__init__.py
+++ uq4pk_src/__init__.py
@@ -1,2 +1,2 @@
-from uq4pk_src import model_grids, plotting, losvds, distribution_function, data, observation_operator, regularisers
-from uq4pk_fit.mcmc import svd_mcmc
+from uq4pk_src import model_grids, plotting, losvds, distribution_function, data, observation_operator, regularisers, \
+    svd_mcmc
